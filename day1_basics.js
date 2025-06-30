24ac2da1946c06fca358ff89d8a1439f93edfd85
strangeverest <strangeverest001@gmail.com>
2025-06-30 21:46:52 +0100

    Day 1 JS practice

---
+++ day1_basics.js
@@ -0,0 +1,14 @@
+// Declare name, age, and isStudent status
+let name = "Strange Everest";
+let age = 23; // Replace with your real age
+let isStudent = true;
+
+// Print all variables
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Is Student:", isStudent);
+
+// What type is each variable?
+console.log(typeof name);      // Should be 'string'
+console.log(typeof age);       // Should be 'number'
+console.log(typeof isStudent); // Should be 'boolean'
